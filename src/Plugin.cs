a9bcf54aed2c8ee6c309727083fb6aa2e6013b73
Zeldack974 <Zeldak974@gmail.com>
2023-12-14 18:13:01 +0400

    update mod with a symbolic link update version to 1.0.2

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -11,7 +11,7 @@ using System.Collections.Generic;
 
 namespace Silkslug
 {
-    [BepInPlugin(MOD_ID, "Silkslug", "1.0.1")]
+    [BepInPlugin(MOD_ID, "Silkslug", "1.0.2")]
     class Plugin : BaseUnityPlugin
     {
         private const string MOD_ID = "zeldak974.silkslug";
