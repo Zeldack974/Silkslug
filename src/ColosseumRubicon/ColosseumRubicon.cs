b1ce920b9dc12af6ade5d5110dd94c118424ea6e
Zeldack974 <Zeldak974@gmail.com>
2024-01-28 13:10:45 +0400

    fix: duplicate Shaw name register feat: VS_E05WrapAround hooks for cr warping

--- src/ColosseumRubicon/ColosseumRubicon.cs
+++ src/ColosseumRubicon/ColosseumRubicon.cs
@@ -1,13 +1,28 @@
 ﻿
+using MoreSlugcats;
 
 namespace Silkslug.ColosseumRubicon
 {
     public static class ColosseumRubicon
     {
-        public class ColosseumRubiconManager
+        public static class ColosseumRubiconManager
         {
-            public ColosseumRubiconManager()
+            public static void Initialize()
             {
+
+            }
+
+            public static void addScript(Room room)
+            {
+                if (room.abstractRoom.name == "SB_E05SAINT")
+                {
+                    ConsoleWrite("Add RoomSpecificScript: " + (room.game.GetStorySession.saveState.saveStateNumber == Plugin.ShawName));
+                    if (room.game.GetStorySession.saveState.saveStateNumber == Plugin.ShawName)
+                    {
+                        room.AddObject(new MSCRoomSpecificScript.VS_E05WrapAround(room));
+                        ConsoleWrite("RoomSpecificScript added");
+                    }
+                }
             }
         }
     }
